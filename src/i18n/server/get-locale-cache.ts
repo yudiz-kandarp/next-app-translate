a81ee6abfdaa010eeb3bca3c538bd85476850b33
yudiz-kuldip <kuldip.dobariya@sportsbuzz.com>
2023-12-18 14:23:33 +0530

    added changes for exporting: - made client and server path for simply import

--- src/i18n/server/get-locale-cache.ts
+++ src/i18n/server/get-locale-cache.ts
@@ -1,4 +1,4 @@
-import { cookies, headers } from 'next/headers'
+import 'next/headers'
 import { cache } from 'react'
 
 const getLocale = cache<() => { current: string | undefined }>(() => ({ current: undefined }))
@@ -14,17 +14,7 @@ export const getLocaleCache = cache(() => {
 	locale = getStaticParamsLocale()
 
 	if (!locale) {
-		try {
-			locale = headers().get('X-Next-Lang')
-
-			if (!locale) {
-				locale = cookies().get('Next-Lang')?.value
-			}
-		} catch (e) {
-			throw new Error(
-				'Could not find locale while pre-rendering page, make sure you called `setStaticParamsLocale` at the top of your pages'
-			)
-		}
+		throw new Error('Could not find locale while pre-rendering page, make sure you called `setStaticParamsLocale` at the top of your pages')
 	}
 
 	return locale
